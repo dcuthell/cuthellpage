d57905b1881ca4467202202681f062767dff9ad2
David Cuthell <dcc1447@gmail.com>
2018-07-24 17:10:36 -0700

    cleaned up home

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Container } from 'reactstrap';
 import './styles.css';
 
 import Logo from '../../img/SamDavid6.png';
@@ -11,19 +11,7 @@ render() {
     <div className="Footer">
 			<Row>
 				<Col sm="3">
-					<img id="footerLogo" src={Logo} alt="footer logo" />
-				</Col>
-				<Col sm="6" className="addressBox">
-					<p><strong>Nashville Classical Ballet Academy</strong></p>
-					<p>123 Main St.</p>
-					<p>Nashville TN 12345</p>
-					<p>503-867-5309</p>
-				</Col>
-				<Col sm="3">
-					<a href="http://www.facebook.com"><i className="mediaIcon fab fa-facebook"></i></a>
-					<a href="http://www.instagram.com"><i className="mediaIcon fab fa-instagram"></i></a>
-					<a href="http://www.twitter.com"><i className="mediaIcon fab fa-twitter"></i></a>
-					<a href="http://www.youtube.com"><i className="mediaIcon fab fa-youtube"></i></a>
+					<h1>Cuthell.com</h1>
 				</Col>
 			</Row>
     </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,40 +16,6 @@ class Home extends Component {
 						<CarouselFrame/>
 					</Col>
 				</Row>
-        <Row>
-          <Col>
-            <Jumbotron id="jumbo1">
-              <h1 className="display-3">Classical Ballet Classes</h1>
-              <hr className="my-2" />
-              <p>This can be a link for the about section</p>
-              <p className="lead">
-                <Button href={"#/About"} color="secondary">Learn More</Button>
-              </p>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Jumbotron id="jumbo2">
-              <p className="lead">Different Classes and Levels of Instuction</p>
-              <hr className="my-2" />
-              <p>This can be a link for the Programs Section</p>
-              <p className="lead">
-                <Button href={"#/Programs"} color="primary">See Classes</Button>
-              </p>
-            </Jumbotron>
-          </Col>
-          <Col>
-            <Jumbotron id="jumbo3">
-              <h1 className="display-5">Support the School</h1>
-							<hr className="my-2" />
-              <p>Here's a link to paypal. A donation page can also be added, as well as a menu item</p>
-              <p className="lead">
-                <Button href={"https://www.paypal.com"} color="success">Donate</Button>
-              </p>
-            </Jumbotron>
-          </Col>
-        </Row>
       </Container>
 		)
   }
